0bc7fa5989d84e4d4d7bec68b502c5de608d2165
SatyamBarle <144716591+SatyamBarle@users.noreply.github.com>
2025-06-15 02:01:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,9 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 # Run Flask app
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
